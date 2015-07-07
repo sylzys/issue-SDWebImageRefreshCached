75c4a7591e403795cc8d3e64d380ebe408f34e45
Syl Zys <sylzys@gmail.com>
2015-07-07 16:44:21 +0200

    issue

---
+++ app/stylesheets/home_screen_stylesheet.rb
@@ -0,0 +1,24 @@
+class HomeScreenStylesheet < ApplicationStylesheet
+
+  def setup
+    # Add stylesheet specific setup stuff here.
+    # Add application specific setup stuff in application_stylesheet.rb
+  end
+
+  def root_view(st)
+    st.background_color = color.white
+  end
+
+  def hello_world(st)
+    st.frame = {t: 100, w: 200, h: 18, centered: :horizontal}
+    st.text_alignment = :center
+    st.color = color.battleship_gray
+    st.font = font.medium
+    st.text = 'Hello World'
+  end
+
+  def image(st)
+    st.frame = "a3:e5"
+  end
+
+end
---
+++ spec/main_spec.rb
@@ -0,0 +1,2 @@
+describe "Application 'issue-SDWebImageRefreshCached'" do
+end
